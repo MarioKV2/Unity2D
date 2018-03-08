83019e00cceba925bf5a2930cf4044f92ca81c85
Making Workmachine <post@makingview.com>
2018-03-08 14:20:50 +0100

    Added bullet trails

--- 2DPlatformer/Assets/Weapon.cs
+++ 2DPlatformer/Assets/Weapon.cs
@@ -8,6 +8,8 @@ public class Weapon : MonoBehaviour {
     public float Damage = 10;
     public LayerMask whatToHit;
 
+    public Transform BulletTrailPrefab;
+
     float timeToFire;
     Transform firePoint;
 
@@ -46,12 +48,18 @@ public class Weapon : MonoBehaviour {
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+        Effect();
         Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.green);
         if (hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Debug.Log("Entity HIT: " + hit.collider.name + " dealt " + Damage + " damage");
         }
+    }
+
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
     }
 
 }
