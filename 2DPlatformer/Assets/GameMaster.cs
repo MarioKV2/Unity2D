04097861529edae672266ad21e3e904b6cc4ba99
Making Workmachine <post@makingview.com>
2018-03-15 11:45:44 +0100

    Added spawn delay

--- 2DPlatformer/Assets/GameMaster.cs
+++ 2DPlatformer/Assets/GameMaster.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer() {
+        Debug.Log("TODO: Add audio while waiting for spawn");
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("TODO: Add spawn particles");
+
+    }
+
     public static void KillPlayer(Player player) {
 
         Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
 
     }
 } 
