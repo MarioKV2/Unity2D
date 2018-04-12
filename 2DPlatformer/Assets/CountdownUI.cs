c1bb1764bbdcc8a4420029d5e822b14d732d5e63
Bård Harald <lol_controll@hotmail.com>
2018-04-12 15:02:13 +0200

    Added UI Counter

---
+++ 2DPlatformer/Assets/CountdownUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownUI : MonoBehaviour {
+
+	float timeLeft = 0.0f;
+
+	public Text myText;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft += Time.deltaTime;
+		myText.text =  timeLeft.ToString ("0");
+	}
+
+	public float ReturnResult()
+	{
+		return timeLeft;
+	}
+}
