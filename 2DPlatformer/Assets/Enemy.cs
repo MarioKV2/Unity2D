8d65cfb4f988215c98e552e8e380a9836edd14a4
Making Workmachine <post@makingview.com>
2018-04-19 14:51:03 +0200

    Added Enemy UI (Healthbar, text)

--- 2DPlatformer/Assets/Enemy.cs
+++ 2DPlatformer/Assets/Enemy.cs
@@ -7,18 +7,48 @@ public class Enemy : MonoBehaviour {
     [System.Serializable]
     public class EnemyStats
     {
+        public int maxHealth = 100;
 
-        public int Health = 100;
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+        }
+
+        public void Init()
+        {
+            curHealth = maxHealth;
+        }
     }
 
     public EnemyStats stats = new EnemyStats();
 
+    [Header("Optional: ")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    private void Start()
+    {
+        stats.Init();
+
+        if(statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+        }
+    }
+    
     public void DamageEnemy(int damage)
     {
-        stats.Health -= damage;
-        if (stats.Health <= 0)
+        stats.curHealth -= damage;
+        if (stats.curHealth <= 0)
         {
             GameMaster.KillEnemy(this);
+        }
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
         }
     }
 
